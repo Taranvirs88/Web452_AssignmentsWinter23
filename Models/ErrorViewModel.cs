61966d4e3f039a65bf452586cc4ea6d087efade4
Taranvirs88 <113557821+Taranvirs88@users.noreply.github.com>
2023-02-16 17:28:47 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rulers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
